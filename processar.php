2bd9894c96589c66007e369fd0f7efd247cadbaf
Thiago <thiago.ferreira164@etec.sp.gov.br>
2025-04-11 14:46:55 -0300

    adicionei comentarios

--- processar.php
+++ processar.php
@@ -2,7 +2,7 @@
 // Verifica se o parâmetro 'name' foi enviado na URL
 if (isset($_GET['nome'])) {
     // Obtém o valor do parâmetro 'name'
-    $nome = htmlspecialchars($_GET['nome']); // Sanitiza o input para evitar XSS
+    $nome = htmlspecialchars($_GET['nome']);
 
     // Retorna uma mensagem de boas-vindas personalizada
     echo "Olá, $nome! Bem-vindo(a) ao sistema.";
